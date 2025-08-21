c09cb31a9c9e6de1786cd00ca6fa84094e3f9b4f
raghuposina11 <posinaraghubabu9177@gmail.com>
2025-08-22 00:35:52 +0530

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import { DATA } from '../data';
-
-export default function Footer() {
-  return (
-    <footer className="border-t py-8 text-center">
-      <p>© {new Date().getFullYear()} {DATA.name}. All rights reserved.</p>
-    </footer>
-  );
-}
